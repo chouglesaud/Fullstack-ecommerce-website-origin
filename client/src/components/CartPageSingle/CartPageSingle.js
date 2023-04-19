ae70ac17bae1c749cc55baae158bbaf48240906f
chouglesaud <saudchougle.sc@gmail.com>
2023-04-20 00:29:52 +0530

    dollor to rupee change

--- client/src/components/CartPageSingle/CartPageSingle.js
+++ client/src/components/CartPageSingle/CartPageSingle.js
@@ -1,58 +1,53 @@
-import React, { useEffect, useState } from "react";
-import "./CartPageSingle.css";
-import { Button, Text } from "@nextui-org/react";
-import { Delete } from "react-iconly";
-import { useDispatch } from "react-redux";
-import { asyncCartEdit, asyncCartGet } from "../../store/cartSlice";
+import React, { useEffect, useState } from 'react';
+import './CartPageSingle.css';
+import { Button, Text } from '@nextui-org/react';
+import { Delete } from 'react-iconly';
+import { useDispatch } from 'react-redux';
+import { asyncCartEdit, asyncCartGet } from '../../store/cartSlice';
 
 export default function CartPageSingle({ data, handleDelete }) {
-  const dispatch = useDispatch()
-  const image = data.images.split(" ~ ")[0];
+  const dispatch = useDispatch();
+  const image = data.images.split(' ~ ')[0];
   const [prodQuantity, setProdQuantity] = useState(data.quantity);
-
 
   const handleAdd = (type) => {
     if (!type && prodQuantity <= 1) {
       setProdQuantity(1);
       return null;
     }
-    type
-      ? setProdQuantity(prodQuantity + 1)
-      : setProdQuantity(prodQuantity - 1);
-    
+    type ? setProdQuantity(prodQuantity + 1) : setProdQuantity(prodQuantity - 1);
   };
 
   useEffect(() => {
-    if(localStorage.getItem('token')) {
-      dispatch(asyncCartGet(localStorage.getItem('token')))
+    if (localStorage.getItem('token')) {
+      dispatch(asyncCartGet(localStorage.getItem('token')));
     }
-
-  }, [dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
-    dispatch(asyncCartEdit({id : data.cart_id, token : localStorage.getItem('token'), quantity : prodQuantity}))
-  }, [prodQuantity, dispatch, data.cart_id])
-  
-  
+    dispatch(asyncCartEdit({ id: data.cart_id, token: localStorage.getItem('token'), quantity: prodQuantity }));
+  }, [prodQuantity, dispatch, data.cart_id]);
 
   return (
-    <div className="cartpage-item">
-      <div className="cartpage-item-left">
-        <img src={image} alt="" />
-        <div className="cartpage-item-detail">
+    <div className='cartpage-item'>
+      <div className='cartpage-item-left'>
+        <img src={image} alt='' />
+        <div className='cartpage-item-detail'>
           <h2>{data.name}</h2>
           <p>
             Size: <span>{data.size}</span>
           </p>
-          <p>Price: <span>{(data.price / 80).toFixed(2)}</span></p>
+          <p>
+            Price: <span>{data.price.toFixed(2)}</span>
+          </p>
         </div>
       </div>
-      <div className="cartpage-item-right">
-        <Button light color="primary" auto>
+      <div className='cartpage-item-right'>
+        <Button light color='primary' auto>
           <Text b size={25} onClick={() => handleAdd(0)}>
             <span>-</span>
           </Text>
-          <Text size={22} color="black">
+          <Text size={22} color='black'>
             {prodQuantity}
           </Text>
           <Text b size={25} onClick={() => handleAdd(1)}>
@@ -65,9 +60,9 @@ export default function CartPageSingle({ data, handleDelete }) {
             handleDelete(data.cart_id);
           }}
           auto
-          color="error"
-          style={{ width: "50px", padding: 0 }}
-          icon={<Delete set="bold" primaryColor="white" />}
+          color='error'
+          style={{ width: '50px', padding: 0 }}
+          icon={<Delete set='bold' primaryColor='white' />}
         />
       </div>
     </div>
--- client/src/pages/CartPage/CartPage.js
+++ client/src/pages/CartPage/CartPage.js
@@ -1,36 +1,35 @@
-import React from "react";
-import "./CartPage.css";
-import { Button } from "@nextui-org/react";
-import CartPageSingle from "../../components/CartPageSingle/CartPageSingle";
-import { Wallet } from "react-iconly";
-import NewsLetter from "../../components/NewsLetter/NewsLetter";
-import ArekFooter from "../../components/ArekFooter/ArekFooter";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { asyncCartDelete, deleteCartItem } from "../../store/cartSlice";
-import StripeCheckout from "react-stripe-checkout";
-import { toast } from "react-toastify";
-import { API_BASE_URL } from "../../config";
+import React from 'react';
+import './CartPage.css';
+import { Button } from '@nextui-org/react';
+import CartPageSingle from '../../components/CartPageSingle/CartPageSingle';
+import { Wallet } from 'react-iconly';
+import NewsLetter from '../../components/NewsLetter/NewsLetter';
+import ArekFooter from '../../components/ArekFooter/ArekFooter';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncCartDelete, deleteCartItem } from '../../store/cartSlice';
+import StripeCheckout from 'react-stripe-checkout';
+import { toast } from 'react-toastify';
+import { API_BASE_URL } from '../../config';
 
-toast.configure()
+toast.configure();
 
 export default function CartPage() {
-  const dispatch = useDispatch()
-  const data = useSelector(state => state.cart.allCartData)
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.cart.allCartData);
   let total = 0;
   let tax = 0;
 
-  const [stripeToken, setStripeToken] = React.useState(null)
+  const [stripeToken, setStripeToken] = React.useState(null);
 
-
-  if(data) {
+  if (data) {
     data.map((item) => {
-      total += ((parseFloat(item.price) / 80).toFixed(3) * parseInt(item.quantity))
-      return null
-    })
-    total = parseFloat(total).toFixed(2)
-    tax = Math.floor((total * 10) / 100)
-    total = parseFloat(total)
+      total += parseFloat(item.price).toFixed(3) * parseInt(item.quantity);
+      return null;
+    });
+    total = parseFloat(total).toFixed(2);
+    tax = Math.floor((total * 10) / 100);
+    total = parseFloat(total);
     total += tax;
   }
 
@@ -39,56 +38,59 @@ export default function CartPage() {
       const res = await fetch(`${API_BASE_URL}/api/checkout/payment`, {
         method: 'POST',
         headers: {
-          'Content-Type' : 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({tokenId : stripeToken.id, amount : total*100})
-      })
+        body: JSON.stringify({ tokenId: stripeToken.id, amount: total * 100 }),
+      });
 
-      const data = await res.json()
-      if(res.ok) {
+      const data = await res.json();
+      if (res.ok) {
         toast.success('Thanks For Purchasing', {
-          position: 'top-center'
-        })
+          position: 'top-center',
+        });
       }
-    }
+    };
 
-    stripeToken && makeRequest()
-  }, [stripeToken,total, tax])
+    stripeToken && makeRequest();
+  }, [stripeToken, total, tax]);
 
-  const onToken = e => setStripeToken(e)
+  const onToken = (e) => setStripeToken(e);
 
   const handleDelete = (id) => {
     dispatch(deleteCartItem(id));
-    dispatch(asyncCartDelete({ token: localStorage.getItem("token"), id }));
+    dispatch(asyncCartDelete({ token: localStorage.getItem('token'), id }));
   };
 
-  if(data) {
-    if(data.length === 0) {
-      return <>
-      <div className="cartpage-container" style={{minHeight : '60vh'}}>
-        <h3 style={{textAlign : 'center' }}>Cart is empty</h3>
-      </div>
-      <NewsLetter />
-      <ArekFooter />
-      </>
+  if (data) {
+    if (data.length === 0) {
+      return (
+        <>
+          <div className='cartpage-container' style={{ minHeight: '60vh' }}>
+            <h3 style={{ textAlign: 'center' }}>Cart is empty</h3>
+          </div>
+          <NewsLetter />
+          <ArekFooter />
+        </>
+      );
     }
   }
 
   return (
     <>
-      <div className="cartpage-container">
-        <div className="cartpage-row">
-          <div className="cartpage-col-left">
+      <div className='cartpage-container'>
+        <div className='cartpage-row'>
+          <div className='cartpage-col-left'>
             <h1>Shopping Cart</h1>
-            <div className="cartpage-item-container">
-              {data && data.map((item, index) => {
-                return <CartPageSingle data={item} key={index} handleDelete={handleDelete} />;
-              })}
+            <div className='cartpage-item-container'>
+              {data &&
+                data.map((item, index) => {
+                  return <CartPageSingle data={item} key={index} handleDelete={handleDelete} />;
+                })}
             </div>
           </div>
-          <div className="cartpage-col-right">
+          <div className='cartpage-col-right'>
             <p>
-              Subtotal <span>{total-tax}</span>
+              Subtotal <span>{total - tax}</span>
             </p>
             <p>
               Estimated Shipping <span>{tax}</span>
@@ -97,25 +99,22 @@ export default function CartPage() {
               Total <span>{total}</span>
             </h3>
             <StripeCheckout
-                alipay={true}
-                bitcoin={true}
-                name="Wookie."
-                image="https://wokiee.reactdemo.hasthemes.com/assets/images/no-placeholder/logo.png"
-                currency="USD"
-                billingAddress
-                shippingAddress
-                amount={total * 100}
-                token={onToken}
-                stripeKey={
-                  'pk_test_51K0KFMGIOFJuO9tJGZUsZARyKWybzpQAL6zcWrZe8awQjyWo75a82Xz7hcg49tVZlxZEMfkQAy47YprQELLDvhWh000YyIa4dO'
-                }
-              >
-                <Button iconRight={<Wallet fill="white" />}>
-                  Proceed to Checkout
-                </Button>
-              </StripeCheckout>
-            <Link to={"/products"}>
-              <Button color="primary" auto ghost style={{ marginTop: "20px" }}>
+              alipay={true}
+              bitcoin={true}
+              name='Wookie.'
+              image='https://wokiee.reactdemo.hasthemes.com/assets/images/no-placeholder/logo.png'
+              currency='INR'
+              billingAddress
+              shippingAddress
+              amount={total}
+              token={onToken}
+              stripeKey={
+                'pk_test_51K0KFMGIOFJuO9tJGZUsZARyKWybzpQAL6zcWrZe8awQjyWo75a82Xz7hcg49tVZlxZEMfkQAy47YprQELLDvhWh000YyIa4dO'
+              }>
+              <Button iconRight={<Wallet fill='white' />}>Proceed to Checkout</Button>
+            </StripeCheckout>
+            <Link to={'/products'}>
+              <Button color='primary' auto ghost style={{ marginTop: '20px' }}>
                 Continue Shopping
               </Button>
             </Link>
